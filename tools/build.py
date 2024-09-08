f96e28ad58d9cbba2ca7f8437b25f4eb53bbe44b
peter88213 <petert88213@gmail.com>
2024-09-09 01:04:31 +0200

    Provide a Python build script.

--- tools/build.py
+++ tools/build.py
@@ -1,9 +1,9 @@
-"""Build a collection novelibre plugin.
+"""Build the nv_progress novelibre plugin package.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the novxlib package.
 
-The novxlib project (see https://github.com/peter88213/novxlib)
+The novxlib project (see see https://github.com/peter88213/novxlib)
 must be located on the same directory level as the nv_progress project. 
 
 Copyright (c) 2024 Peter Triesberger
@@ -12,29 +12,30 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
 import sys
+
 sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
-import inliner
+from package_builder import PackageBuilder
 
-SOURCE_DIR = '../src/'
-TEST_DIR = '../test/'
-SOURCE_FILE = f'{SOURCE_DIR}nv_progress.py'
-TEST_FILE = f'{TEST_DIR}nv_progress.py'
-NVLIB = 'nvlib'
-NV_PATH = '../../novelibre/src/'
-NOVXLIB = 'novxlib'
-NOVX_PATH = '../../novxlib/src/'
+VERSION = '4.2.2'
 
 
-def inline_modules():
-    inliner.run(SOURCE_FILE, TEST_FILE, 'nvprogresslib', '../../nv_progress/src/')
-    inliner.run(TEST_FILE, TEST_FILE, NVLIB, NV_PATH)
-    inliner.run(TEST_FILE, TEST_FILE, NOVXLIB, NOVX_PATH)
-    print('Done.')
+class PluginBuilder(PackageBuilder):
+
+    PRJ_NAME = 'nv_progress'
+    LOCAL_LIB = 'nvprogresslib'
+    GERMAN_TRANSLATION = True
+
+    def __init__(self, version):
+        super().__init__(version)
+        self.iconDir = '../icons'
+
+    def add_extras(self):
+        self.add_icons()
 
 
 def main():
-    os.makedirs(TEST_DIR, exist_ok=True)
-    inline_modules()
+    pb = PluginBuilder(VERSION)
+    pb.run()
 
 
 if __name__ == '__main__':
