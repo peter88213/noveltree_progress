398ed01d29dce94f3423a4324e757e8cff76b976
peter88213 <petert88213@gmail.com>
2024-10-22 10:43:08 +0200

    Refactor, replacing global constants with class constants.

--- src/nv_progress.py
+++ src/nv_progress.py
@@ -22,19 +22,6 @@ from novxlib.ui.set_icon_tk import set_icon
 from nvlib.plugin.plugin_base import PluginBase
 from nvprogresslib.progress_viewer import ProgressViewer
 
-APPLICATION = _('Daily progress log')
-PLUGIN = f'{APPLICATION} plugin v@release'
-
-SETTINGS = dict(
-    window_geometry='510x440',
-    date_width=100,
-    wordcount_width=100,
-    wordcount_delta_width=100,
-    totalcount_width=100,
-    totalcount_delta_width=100,
-)
-OPTIONS = {}
-
 
 class Plugin(PluginBase):
     """novelibre daily progress log viewer plugin class."""
@@ -43,19 +30,30 @@ class Plugin(PluginBase):
     DESCRIPTION = 'A daily progress log viewer'
     URL = 'https://github.com/peter88213/nv_progress'
 
+    FEATURE = _('Daily progress log')
+    SETTINGS = dict(
+        window_geometry='510x440',
+        date_width=100,
+        wordcount_width=100,
+        wordcount_delta_width=100,
+        totalcount_width=100,
+        totalcount_delta_width=100,
+    )
+    OPTIONS = {}
+
     def disable_menu(self):
         """Disable menu entries when no project is open.
         
         Overrides the superclass method.
         """
-        self._ui.toolsMenu.entryconfig(APPLICATION, state='disabled')
+        self._ui.toolsMenu.entryconfig(self.FEATURE, state='disabled')
 
     def enable_menu(self):
         """Enable menu entries when a project is open.
         
         Overrides the superclass method.
         """
-        self._ui.toolsMenu.entryconfig(APPLICATION, state='normal')
+        self._ui.toolsMenu.entryconfig(self.FEATURE, state='normal')
 
     def install(self, model, view, controller, prefs=None):
         """Add a submenu to the 'Tools' menu.
@@ -82,8 +80,8 @@ class Plugin(PluginBase):
             configDir = '.'
         self.iniFile = f'{configDir}/progress.ini'
         self.configuration = self._mdl.nvService.make_configuration(
-            settings=SETTINGS,
-            options=OPTIONS
+            settings=self.SETTINGS,
+            options=self.OPTIONS
             )
         self.configuration.read(self.iniFile)
         self.kwargs = {}
@@ -91,8 +89,8 @@ class Plugin(PluginBase):
         self.kwargs.update(self.configuration.options)
 
         # Create an entry in the Tools menu.
-        self._ui.toolsMenu.add_command(label=APPLICATION, command=self._start_viewer)
-        self._ui.toolsMenu.entryconfig(APPLICATION, state='disabled')
+        self._ui.toolsMenu.add_command(label=self.FEATURE, command=self._start_viewer)
+        self._ui.toolsMenu.entryconfig(self.FEATURE, state='disabled')
 
     def on_close(self):
         """Close the window.
@@ -129,6 +127,6 @@ class Plugin(PluginBase):
                 return
 
         self._progress_viewer = ProgressViewer(self, self._mdl)
-        self._progress_viewer.title(f'{self._mdl.novel.title} - {PLUGIN}')
+        self._progress_viewer.title(f'{self._mdl.novel.title} - {self.FEATURE} plugin v@release')
         set_icon(self._progress_viewer, icon='wLogo32', default=False)
 
